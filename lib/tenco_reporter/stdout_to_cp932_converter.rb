8dd729eaa8f74e544e5f1ed86717cddfec07787e
nanashi.hatch <nanashi.hatch@gmail.com>
2018-12-03 14:57:04 +0900

    for Windows 10 October 2018 Update: to write stdout, string should be started with ASCII code
    Signed-off-by: nanashi.hatch <nanashi.hatch@gmail.com>

--- lib/tenco_reporter/stdout_to_cp932_converter.rb
+++ lib/tenco_reporter/stdout_to_cp932_converter.rb
@@ -7,7 +7,9 @@ if !defined?($stdout._write)
   class << $stdout
     alias :_write :write
     def write(str)
-      _write NKF.nkf('-sxm0 --cp932', str.to_s)
+	  # for Windows 10 October 2018 Update
+	  # to write stdout, string should be started with ASCII code
+      _write NKF.nkf('-sxm0 --cp932', "\x20\x08#{str.to_s}")
     end
   end
 end
